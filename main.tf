b5c18f9f4315cb5211aca40a36e0173efe404e11
po3xmgroup <pablo.ojeda@3xmgroup.com>
2022-01-23 22:46:28 -0300

    add organization and workspace name

--- main.tf
+++ main.tf
@@ -2,7 +2,20 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "3xmgroup"
+
+    workspaces {
+      name = "po3xmgroup-ec2-static-website"
     }
   }
 }
